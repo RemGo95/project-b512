27248cceb55100c11f76706adc6e8fc8b21f5f6e
Remigius <37455393+RemGo95@users.noreply.github.com>
2022-03-14 18:36:06 +0100

    Add files via upload

---
+++ Scripts/OperatorRotations.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OperatorRotations : MonoBehaviour
+{
+    // horizontal rotation speed
+    public float horizontalSpeed = 1f;
+    // vertical rotation speed
+    public float verticalSpeed = 1f;
+    private float xRotation = 0.0f;
+    private float yRotation = 0.0f;
+    private Camera cam;
+
+    void Start()
+    {
+        //cam = Camera.main;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * horizontalSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * verticalSpeed;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        //cam.transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
+
+        Debug.Log((xRotation, yRotation).ToString());
+    }
+}
